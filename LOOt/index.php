63b498ea1a226c5825530691a29041b5b1afc9eb
Jade <jadeluany100@gmail.com>
2020-01-18 01:49:52 -0300

    LOOt - css

---
+++ LOOt/index.php
@@ -0,0 +1,8 @@
+<?php
+	include 'views/header.php';
+	include 'views/navbar.php';
+	include 'views/footer.php';
+	include 'views/jumbotron.php';
+	include 'views/form.php';
+	include 'views/rodape.php';
+?>
